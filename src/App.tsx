978935fa95d62f9c974120cc3aac249fd4dab4e1
Luis Ruediger <luisruediger27@gmail.com>
2023-05-24 17:33:55 -0300

    create context

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import { Router } from './Router'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { CounterContextProvider } from './contexts/CounterContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CounterContextProvider>
+          <Router />
+        </CounterContextProvider>
       </BrowserRouter>
 
       <GlobalStyle />
---
+++ src/contexts/CounterContext.tsx
@@ -0,0 +1,35 @@
+import { ReactNode, createContext, useState } from 'react'
+
+interface CounterContextProviderProps {
+  children: ReactNode
+}
+
+interface CounterContextType {
+  counter: number
+  handleDecreaseCounter: () => void
+  handleIncreaseCounter: () => void
+}
+
+export const CounterContext = createContext({} as CounterContextType)
+
+export function CounterContextProvider({
+  children,
+}: CounterContextProviderProps) {
+  const [counter, setCounter] = useState(0)
+
+  function handleIncreaseCounter() {
+    setCounter((state) => state + 1)
+  }
+
+  function handleDecreaseCounter() {
+    setCounter((state) => state - 1)
+  }
+
+  return (
+    <CounterContext.Provider
+      value={{ counter, handleIncreaseCounter, handleDecreaseCounter }}
+    >
+      {children}
+    </CounterContext.Provider>
+  )
+}
